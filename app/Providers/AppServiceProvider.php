eee14a15d5b63e6920caa75a7a4283e6651d812c
EL BAGRESON <juanfelipebarrioshidalgo@gmail.com>
2024-03-16 18:18:52 -0500

    hosting

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\URL; // Importa la clase URL correctamente
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https'); // Usa la clase URL sin el prefijo \
+        }
+    }
+}
